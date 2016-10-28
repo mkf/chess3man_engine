67dde9def9fe32be97f32a2aa488cf3e14dd4f84
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-10-28 21:50:28 +0200

    checkempties instead of continousemptyroute

--- lib/possib.dart
+++ lib/possib.dart
@@ -3,12 +3,7 @@ library chess3man.engine.possib;
 import "board.dart";
 import "pos.dart";
 
-bool continousemptyroute(Pos from, Pos to, Board b, ContinousVector v) {
-  Pos a = from;
-  for (final ContinousVector u in v.units(from.rank)) {
-    a = u.addTo(a);
-    if (a == to) return true;
-    if (b.nePos(a)) return false;
-  }
-  throw new AssertionError();
+bool checkempties(Pos from, Board b, Vector v) {
+  for (final Pos a in v.emptiesFrom(from)) if (b.nePos(a)) return false;
+  return true;
 }
