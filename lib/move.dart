7c8932584b320a28329d9f7c9ac40e6cfa6fb811
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-10-29 19:57:02 +0200

    Working on move

--- lib/move.dart
+++ lib/move.dart
@@ -3,6 +3,7 @@ library chess3man.engine.move;
 import 'pos.dart';
 import 'state.dart';
 import 'board.dart';
+import 'possib.dart';
 
 class Move {
   final Pos from;
@@ -14,4 +15,40 @@ class Move {
   Fig get what => fromsq.fig;
   Square get tosq => before.board.gPos(to);
   Fig get alreadyThere => tosq.fig;
+  Future<bool> possible() async {
+    //TODO: Pos.correct?
+    if (fromsq.empty) throw new NothingHereAlreadyError(this);
+    if (what.color != before.movesnext)
+      throw new ThatColorDoesNotMoveNowError(this, what.color);
+    if (!(await possib(
+        from,
+        before.board,
+        bec,
+        before.moatsstate,
+        before.enpassant,
+        before.castling))) throw new ImpossibleMoveError(this);
+    return true;
+  }
+  Future<State> after() async {
+    assert (possible());
+    //TODO
+  }
+}
+
+class IllegalMoveError extends Exception {
+  final Move m;
+  IllegalMoveError(this.m);
+}
+
+class NothingHereAlreadyError extends IllegalMoveError {
+  NothingHereAlreadyError(Move m) : super(m);
+}
+
+class ThatColorDoesNotMoveNowError extends IllegalMoveError {
+  final Color c;
+  ThatColorDoesNotMoveNowError(Move m, this.c) : super(m);
+}
+
+class ImpossibleMoveError extends IllegalMoveError {
+  ImpossibleMoveError(Move m) : super(m);
 }
