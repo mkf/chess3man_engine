510ee0b4391fcae4e88b83aed4b7ed26edc3bba6
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-10-31 00:21:50 +0100

    First test

--- test/move_test.dart
+++ test/move_test.dart
@@ -5,6 +5,11 @@ import 'package:test/test.dart';
 
 main() {
   test("simple gen no panic", () {
-
+    State newState = new State.newGame();
+    Pos from = new Pos(1, 0);
+    Pos to = new Pos(3, 0);
+    Iterable<Vector> vecs = newState.b.gPos(from).fig.vecs_ns(from, to);
+    Move first = new Move(from, vecs.first, newState);
+    expect(first.after(),isNotNull);
   });
 }
