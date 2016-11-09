83859be5ab29055537ac30bf47abac86b84512ca
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-11-09 09:51:28 +0100

    proper _amftinit

--- lib/amft.dart
+++ lib/amft.dart
@@ -5,15 +5,15 @@ import "package:built_collection/built_collection.dart";
 
 BuiltMap<Pos, BuiltSet<Pos>> _amftinit() {
   MapBuilder<Pos, BuiltSet<Pos>> ourmain = new MapBuilder<Pos, BuiltSet<Pos>>();
-  Board b = new Board();
   for (Pos from = new Pos.zero(); from != null; from = from.next()) {
     SetBuilder<Pos> ourslave = new SetBuilder<Pos>();
     for (Pos to = new Pos.zero(); to != null; to = to.next()) {
-      if (from == new Pos(5, 10)) {
-        // ↓↑ just senseless placeholders
-        b.empt(new Pos(5, 10));
-        ourslave.add(to);
-      }
+      if (from
+                  .continousVectorsTo(to)
+                  .where((ContinousVector elem) => elem != null)
+                  .length >
+              0 ||
+          from.knightVectorTo(to) != null) ourslave.add(to);
     }
     ourmain[from] = ourslave.build();
   }
